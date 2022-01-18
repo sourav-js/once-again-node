8013c225db85b8e747c66b67f4a8d59ea4235d76
sourav-js <76809746+sourav-js@users.noreply.github.com>
2022-01-19 01:20:49 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,7 +1,7 @@
 require("dotenv");
 var express= require("express"),
 app=express();
-var port= process.env.PORT || 4000
+var port= process.env.PORT || 4000;
 app.get("/",function(req,res){
 
 	res.send("again hosted successfully")
